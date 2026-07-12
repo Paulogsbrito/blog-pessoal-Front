878ae0f8d87b94c0224c35c46c25fa2986c7a6c1
Paulo <paulogsbrito@gmail.com>
2026-07-12 03:25:54 -0300

    implementada a conexão do backend e realizado login e cadastro de usuario

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
 	return (
 		<>
+		<AuthProvider>
 		  <BrowserRouter>
 			<Navbar />
 			<div className="min-h-[80vh]">
@@ -19,6 +21,7 @@ function App() {
 			</div>
 			<Footer />
 			</BrowserRouter>
+			</AuthProvider>
 		</>
 	)
 }
